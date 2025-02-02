0f3e794be2b10e349ecd228090bebac83a3650e2
yash-makwana <97906812+yash-makwana@users.noreply.github.com>
2025-02-02 18:56:22 +0530

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
         <div className=".container header__container">
             <h5>Hello, I'm</h5>
             <h1>Yash Makwana</h1>
-            <h5 className="text-light">Frontend Developer</h5>
+            <h5 className="text-light">Data Scientist</h5>
             <CTA />
             <HeaderSocial />
             <div className="me">
