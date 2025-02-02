44eb9a4c52de4421e0f3e558844d703bc6aa49ee
yash-makwana <97906812+yash-makwana@users.noreply.github.com>
2025-02-02 19:36:46 +0530

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './About.css'
-import Image from '../../assets/Makwana.jpg'
+import Image from '../../assets/about_me.jpg'
 import {FaAward} from 'react-icons/fa';
 import {FiUsers} from 'react-icons/fi';
 import {VscFolderLibrary} from 'react-icons/vsc';
