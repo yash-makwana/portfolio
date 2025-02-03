29db01756e2cba47f6bafce01513e7a959298a0b
yash-makwana <97906812+yash-makwana@users.noreply.github.com>
2025-02-03 20:58:44 +0530

    Update portfolio_data.js

--- src/components/portfolio/portfolio_data.js
+++ src/components/portfolio/portfolio_data.js
@@ -5,34 +5,70 @@ import Portfolio4 from '../../assets/4.png'
 import Portfolio5 from '../../assets/5.png'
 const data = [
     {
-        id : 1,
-        img : Portfolio1,
-        title : 'This is prototype item title',
-        github : 'https://github.com/yash-makwana/LNGC'
+        id: 1,
+        img: '', // No image available
+        title: 'Design to Cost (D2C)',
+        github: 'https://github.com/yash-makwana',
+        description: 'Implemented a cost-efficient predictive model pipeline using Dataiku, focusing on data cleaning, statistical analysis (ANOVA), and machine learning optimization.'
     },
     {
-        id : 2,
-        img : Portfolio2,
-        title : 'This is prototype item title',
-        github : 'https://github.com/yash-makwana/Non-redundant-cover-set-formation-to-improve-full-coverage-in-wsns'
+        id: 2,
+        img: '', // No image available
+        title: 'Smart Troubleshooting (STS)',
+        github: 'https://github.com/yash-makwana',
+        description: 'Developed an NLP-based system to match queries with past issues, improving problem resolution efficiency. Optimized code for performance and resource management.'
     },
     {
-        id : 3,
-        img : Portfolio3,
-        title : 'This is prototype item title',
-        github : 'https://github.com/yash-makwana/MugChat'
+        id: 3,
+        img: '', // No image available
+        title: 'Tender Benchmark (TBT)',
+        github: 'https://github.com/yash-makwana',
+        description: 'Built a cost estimation pipeline using regression models (Linear, SVM, XGBoost, etc.). Optimized predictions using R² score, MAPE, and MAE.'
     },
     {
-        id : 4,
-        img : Portfolio4,
-        title : 'This is prototype item title',
-        github : 'https://github.com/yash-makwana/Solider-s-Bay'
-    },
+        id: 4,
+        img: '', // No image available
+        title: 'More Projects on GitHub',
+        github: 'https://github.com/yash-makwana',
+        description: 'Explore more of my projects on GitHub, covering machine learning, NLP, and data science applications.'
+    }
     {
         id : 5,
+        img : Portfolio1,
+        title : 'This is prototype item title',
+        github : 'https://github.com/yash-makwana/LNGC',
+        description: 'L N Global Consultancy provides training and placement services to students in all majors from first-year to graduate-level students throughout their college careers and professional growth.We provide advice to students, offer technical and non-technical development programs, and conduct events such as workshops and industry fairs.'
+    },
+    {
+        id : 6,
+        img : Portfolio2,
+        title : 'This is prototype item title',
+        github : 'https://github.com/yash-makwana/Non-redundant-cover-set-formation-to-improve-full-coverage-in-wsns',
+        description: 'Non redundant cover set formation to improve full coverage in WSNs, I have created an algorithm that helps to increase the lifetime of a network. In the algorithm, sensors are divided in such a way that they cover all the targets without being a sensor redundant so that the remaining sensors can be used in the future for the full coverage.'
+        
+    },
+    {
+        id : 7,
+        img : Portfolio3,
+        title : 'This is prototype item title',
+        github : 'https://github.com/yash-makwana/MugChat',
+        description: 'MugChat is a small social media platform which is developed by me with the help of HTML,CSS,JS and PHP for the backend connectivity. In Mugchat user can upload the picture and connect with other and check others profile as well, In Mugchat user can become friends just like facebook and also they can communicate with this amazing platform.
+
+'
+    },
+    {
+        id : 8,
+        img : Portfolio4,
+        title : 'This is prototype item title',
+        github : 'https://github.com/yash-makwana/Solider-s-Bay',
+        description: 'Soilders Bay is a small charity website which is developed by me with the help of HTML,CSS and JS. In this website I have integrated the payment gateway with the help of this user can transfer the money to the soliders account which helps the soiders and his/her family for better life. This website encourage more people to become part of Indian army.'
+    },
+    {
+        id : 9,
         img : Portfolio5,
         title : 'This is prototype item title',
-        github : 'https://github.com/yash-makwana/portfolio'
+        github : 'https://github.com/yash-makwana/portfolio',
+        description: 'This is my portfolio which is created by me with the help of the HTML, CSS, JS and mainly with the react components. This portfolio tells about myself and the projects which are created by me or been in a team where I helped other to complete the projects, You can contact me with the help form, WhatsApp or messenger for interview or freelancing.'
     }
 ]
 
