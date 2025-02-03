ffe8a86b7d20ad26703e809f40f718a9a525cc8e
yash-makwana <97906812+yash-makwana@users.noreply.github.com>
2025-02-03 20:59:09 +0530

    Update Portfolio.jsx

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import './Portfolio.css'
-import Data from './portfolio_data'
+import React from 'react';
+import './Portfolio.css';
+import Data from './portfolio_data';
 
 const Portfolio = () => {
     return (
@@ -10,25 +10,24 @@ const Portfolio = () => {
 
             <div className="container portfolio_container">
                 {
-                    Data.map(({id,img,title,github,demo}) => {
-                    return (
-                        <article key={id} className="portfolio_item">
-                    <div className="portfolio_item_image">
-                        <img src={img} alt="Image of Project"/>
-                    </div>
-                    <h3>{title}</h3>
-                    <div className="portfolio_item-cta">
-                        <a href={github} className="btn " target="_blank">GitHub</a>
-                    </div>
-                </article>
-                    );
-                })
+                    Data.map(({ id, img, title, github, description }) => {
+                        return (
+                            <article key={id} className="portfolio_item">
+                                <div className="portfolio_item_image">
+                                    {img ? <img src={img} alt="Project Thumbnail" /> : <div className="placeholder">No Image Available</div>}
+                                </div>
+                                <h3>{title}</h3>
+                                <p>{description}</p>
+                                <div className="portfolio_item-cta">
+                                    <a href={github} className="btn" target="_blank" rel="noopener noreferrer">GitHub</a>
+                                </div>
+                            </article>
+                        );
+                    })
                 }
             </div>
         </section>
-            
-        
-    )
-}
+    );
+};
 
-export default Portfolio
+export default Portfolio;
